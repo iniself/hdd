3fa546a75304cef4d3589f622f9fba1bd986efc1
Metaer <2839905270@qq.com>
2025-10-24 16:40:35 +0800

    Update type_protocol.py

--- hdd/__init__.py
+++ hdd/__init__.py
@@ -1,3 +1,3 @@
 from hdd.Core import core
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
--- hdd/templates/utils/type_protocol.py
+++ hdd/templates/utils/type_protocol.py
@@ -1,5 +1,4 @@
 from typing import Protocol, overload, Literal, TYPE_CHECKING
-from hdd.service.config import ConfigManager
 
 if TYPE_CHECKING:
     from hdd.service.config import ConfigManager
